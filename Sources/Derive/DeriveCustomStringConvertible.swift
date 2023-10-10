f176861de83d12614e0a3b1973d4f2403af7ee1e
fwcd <fwcdmail@gmail.com>
2023-10-10 17:25:32 +0100

    Remove outdated comments

--- Sources/Derive/DeriveCustomStringConvertible.swift
+++ Sources/Derive/DeriveCustomStringConvertible.swift
@@ -1,8 +1,3 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 /// Synthesizes a `CustomStringConvertible` conformance for `RawRepresentable`.
 @attached(extension, conformances: CustomStringConvertible, names: named(description))
 public macro DeriveCustomStringConvertible() = #externalMacro(module: "DeriveMacros", type: "DeriveCustomStringConvertibleMacro")
-
-// TODO: DeriveOptionSetDescription
