27bbf2edf9ae6f3e750d5ff85ca4fb5420ea16c9
fwcd <fwcdmail@gmail.com>
2023-10-13 10:47:44 +0100

    Re-add `DeriveClient`
    Xcode unfortunately doesn't show us macro expansions in snippets (yet).
    
    This partially reverts commit 143f423235849ca4e0bbb2603a2b8078b4e8b44c.

---
+++ Sources/DeriveClient/main.swift
@@ -0,0 +1,13 @@
+import Derive
+
+@DeriveCustomStringConvertible
+struct Weather: Equatable, RawRepresentable {
+    let rawValue: Int
+
+    static let sunny = Self(rawValue: 0)
+    static let rainy = Self(rawValue: 1)
+    static let cloudy = Self(rawValue: 2)
+}
+
+print("The weather is \(Weather.sunny)")
+print("...or maybe \(Weather.cloudy)")
